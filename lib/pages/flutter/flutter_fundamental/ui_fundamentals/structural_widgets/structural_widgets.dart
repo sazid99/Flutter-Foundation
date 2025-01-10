c8a6f8ec78cf3e3e856b19c7ed31404142093fb3
Sazid <mahmuduzzamansazid@gmail.com>
2025-01-10 23:00:01 +0600

    new commit

--- lib/pages/flutter/flutter_fundamental/ui_fundamentals/structural_widgets/structural_widgets.dart
+++ lib/pages/flutter/flutter_fundamental/ui_fundamentals/structural_widgets/structural_widgets.dart
@@ -11,64 +11,76 @@ class StructuralWidgets extends StatelessWidget {
     final center = MainAxisAlignment.center;
     return Scaffold(
       appBar: CustomAppbar(text: "Structural Widgets"),
-      body: Center(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: center,
-              spacing: 20,
-              children: [
-                Row(
-                  spacing: 20,
+      body: ListView(
+        children: [
+          Center(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Column(
                   mainAxisAlignment: center,
+                  spacing: 20,
                   children: [
-                    CustomElevatedButton(
-                        size: 22,
-                        text: "1. MaterialApp",
-                        path: () => HomePage()),
-                    CustomElevatedButton(
-                        size: 22, text: "2. Scaffold", path: () => HomePage()),
+                    Wrap(
+                      runSpacing: 10,
+                      spacing: 20,
+                      children: [
+                        CustomElevatedButton(
+                            size: 22,
+                            text: "1. MaterialApp",
+                            path: () => HomePage()),
+                        CustomElevatedButton(
+                            size: 22,
+                            text: "2. Scaffold",
+                            path: () => HomePage()),
+                      ],
+                    ),
+                    Wrap(
+                      runSpacing: 10,
+                      spacing: 20,
+                      children: [
+                        CustomElevatedButton(
+                            size: 22,
+                            text: "3. AppBar",
+                            path: () => HomePage()),
+                        CustomElevatedButton(
+                            size: 22, text: "4. body", path: () => HomePage()),
+                      ],
+                    ),
+                    Wrap(
+                      runSpacing: 10,
+                      spacing: 20,
+                      children: [
+                        CustomElevatedButton(
+                            size: 22,
+                            text: "5. bottomNavigationBar",
+                            path: () => HomePage()),
+                        CustomElevatedButton(
+                            size: 22,
+                            text: "6. Container",
+                            path: () => HomePage()),
+                      ],
+                    ),
+                    Wrap(
+                      runSpacing: 10,
+                      spacing: 20,
+                      children: [
+                        CustomElevatedButton(
+                            size: 22,
+                            text: "6. Row and Column",
+                            path: () => HomePage()),
+                        CustomElevatedButton(
+                            size: 22,
+                            text: "8. ListView",
+                            path: () => HomePage()),
+                      ],
+                    ),
                   ],
                 ),
-                Row(
-                  spacing: 20,
-                  mainAxisAlignment: center,
-                  children: [
-                    CustomElevatedButton(
-                        size: 22, text: "3. AppBar", path: () => HomePage()),
-                    CustomElevatedButton(
-                        size: 22, text: "4. body", path: () => HomePage()),
-                  ],
-                ),
-                Row(
-                  spacing: 20,
-                  mainAxisAlignment: center,
-                  children: [
-                    CustomElevatedButton(
-                        size: 22,
-                        text: "5. bottomNavigationBar",
-                        path: () => HomePage()),
-                    CustomElevatedButton(
-                        size: 22, text: "6. Container", path: () => HomePage()),
-                  ],
-                ),
-                Row(
-                  spacing: 20,
-                  mainAxisAlignment: center,
-                  children: [
-                    CustomElevatedButton(
-                        size: 22,
-                        text: "6. Row and Column",
-                        path: () => HomePage()),
-                    CustomElevatedButton(
-                        size: 22, text: "8. ListView", path: () => HomePage()),
-                  ],
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
